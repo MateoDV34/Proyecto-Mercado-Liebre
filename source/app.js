6dbb34d33f06754dc3eb879f65d5bb35189e6ad3
MateoDV34 <matedellavedova@gmail.com>
2022-06-16 20:47:49 -0300

    mensajito

--- source/app.js
+++ source/app.js
@@ -5,9 +5,7 @@ const port = process.env.PORT  || 2020;
 
 app.listen(port, () => console.log('Abriendo el servidor'+port));
 
-const public = path.resolve(__dirname, '../public');
-
-app.use(express.static(public));
+app.use(express.static('public'));
 
 app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
 app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views/register.html')))
